24029f2e33c0366f6dd512c2a1898fae6d66bde7
Himself65 <himself65@outlook.com>
2022-04-16 13:51:48 -0500

    feat: fix errors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  reactStrictMode: true,
-};
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  unstable_staticImage: true
+});
+module.exports = withNextra();
